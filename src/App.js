45043130fc2a9e35ce130c4bf74ebcb62eccf1b4
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-09-08 15:53:31 +0600

    added sidebar into dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import { Sidebar } from './modules/sidebar/components';
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Sidebar />
-      </div>
+      </Container>
     );
   }
 }
